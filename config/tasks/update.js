44b7eefd232b7a0ba106fcca7803d0adc00fc139
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-16 16:52:20 +0530

    Initial commit
    Path: config/tasks/update.js
    Why? – To automate the updating of our "package.json" packages with a grunt task.
    Usage: $ npm install grunt-dev-update --save-dev

---
+++ config/tasks/update.js
@@ -0,0 +1,34 @@
+/*!
+ * CapsidDB®  : A Capsid proteins and their associated functional genes database.
+ * @version   : [1.0.0]
+ * ______________________________________________________________________________
+ * CBBL®
+ * ~~~~~
+ * Computational Biology and Bioinformatics Laboratory
+ * Υπολογιστικής Βιολογίας και Βιοπληροφορικής Εργαστήριο
+ * ______________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ______________________________________________________________________________
+ * @date      : 15-May-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Grunt
+ * @require   : grunt-dev-update
+ * ______________________________________________________________________________
+ *      Licensed under the Apache License, Version 2.0, (the "License");
+ *      you may not use this file except in compliance with the License.
+ *                  You may obtain a copy of the License at:
+ *                 http://www.apache.org/licenses/LICENSE-2.0
+ *                                     or
+ *          https://github.com/CapsidDB/CapsidDB/blob/master/LICENSE
+ *    Unless required by applicable law or agreed to in writing, software
+ *     distributed under the License is distributed on an "AS-IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *                        limitations under the License.
+ * ______________________________________________________________________________
+ */
