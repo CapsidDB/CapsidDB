b65465f14861a87f87c9615bc45f02326765ecf3
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-16 16:10:17 +0530

    Initial commit
    verify.sh, to --/verify/-- the builds after completion of "Grunt-Task(s)", for "CapsidDB®".
    Usage: $ sh verify.sh

---
+++ verify.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+
+# ============================= CapsidDB® =============================
+#   A Capsid proteins and their associated functional genes database.
+#                                 CBBL®
+#           Computational Biology and Bioinformatics Laboratory
+#         Υπολογιστικής Βιολογίας και Βιοπληροφορικής Εργαστήριο
+#         Handcrafted by Prabhat Kumar, http://prabhatkumar.org/.
+# Copyright © 2014 - 2016, Sequømics Corporation. All rights reserved.
+# =====================================================================
